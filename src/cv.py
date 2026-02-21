3f938fa635ac1cfa18ef4aeeedccc1991447505e
hope <ogidanhope1@gmail.com>
2026-02-21 13:24:25 +0100

    workflow beginning

---
+++ src/cv.py
@@ -0,0 +1,43 @@
+import asyncio
+from random import random
+
+from llama_index.core.workflow import StartEvent, StopEvent, Workflow, step, Event
+
+
+class ProcessingEvent(Event):
+    intermediate_result: str
+
+class LoopEvent(Event):
+    loop_output: str
+
+
+
+
+class CVWorkflow(Workflow):
+    @step()
+    async def start(self, ev: StartEvent | LoopEvent) -> ProcessingEvent | LoopEvent:
+       if random.seed(0, 1) == 0:
+            print("Bad thing happened")
+            return LoopEvent(loop_output="Back to step one.")
+       else:
+            print("Good thing happened")
+            return ProcessingEvent(intermediate_result="First step complete.")
+
+    @step()
+    async def step_2(self, ev: ProcessingEvent) -> StopEvent:
+        final_result = f"Finished processing: {ev.intermediate_result}"
+        return StopEvent(result=final_result)
+
+
+
+
+# 2. Execution logic
+async def main():
+    cv_workflow = CVWorkflow(timeout=10, verbose=True) # verbose=True helps debugging
+    # We pass the input data inside the run() method
+    result = await cv_workflow.run()
+    print(f"Final Result: {result}")
+
+if __name__ == "__main__":
+    asyncio.run(main())
+
